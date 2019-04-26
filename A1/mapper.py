94bea67f38924be4c530205334110b8d6957a516
Alina Orozco <alina.orozco12@gmail.com>
2019-04-26 07:02:18 -0700

    modified files

--- A1/mapper.py
+++ A1/mapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 """"mapper.py"""
 import re
 import sys
--- A1/reducer.py
+++ A1/reducer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 """reducer.py"""
 
 from operator import itemgetter
